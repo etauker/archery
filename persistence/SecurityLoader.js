9b982cb55005dbfbe01f6c2e4ad7bbecae427ab5
Tautvydas Kersulis <t.kersulis1@nuigalway.ie>
2018-09-13 00:19:41 +0100

    Added SecurityUserManager template.

---
+++ persistence/SecurityLoader.js
@@ -0,0 +1,15 @@
+/**
+*   Loads data related to security
+*/
+
+class SecurityLoader {
+    constructor() {
+        console.log("SecurityLoader is being constructed");
+    }
+}
+
+SecurityLoader.prototype.getUser = function(oUser) {
+    // TODO: Return a user using properties corresponding to the provided object
+}
+
+module.exports = SecurityLoader;
