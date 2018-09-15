88a8b9f2156644782af1bb5d524beeca8b9a157c
Tautvydas Kersulis <etauker@gmail.com>
2018-09-15 22:20:00 +0100

    Began work on SecurityPersistenceManager.
    Added unit tests.
    Added .end-template file.

--- persistence/SecurityPersistenceManager.js
+++ persistence/SecurityPersistenceManager.js
@@ -2,11 +2,15 @@
 *   Loads and saves data related to security.
 */
 
+var mysql = require('mysql');
+// var mysql = require('promise-mysql');
+const util = require('util');
 class SecurityPersistenceManager {
 
     constructor(oParams) {
         // Set class variables
-        let oDatabase = process.env.database || {};
+        oParams = oParams || {};
+        let oDatabase = process.env ? (process.env["com.etauker.security.db"] || {}) : {};
         this.host = oParams.host || oDatabase.host || 'localhost';
         this.user = oParams.user || oDatabase.user;
         this.password = oParams.password || oDatabase.password;
@@ -16,6 +20,13 @@ class SecurityPersistenceManager {
         if (!this.user) this._missingParameter("user");
         if (!this.password) this._missingParameter("password");
         if (!this.database) this._missingParameter("database");
+        this.state = "progress";
+        this.connection = mysql.createConnection({
+            host     : this.host,
+            user     : this.user,
+            password : this.password,
+            database : this.database
+        });
     }
 }
 
@@ -23,8 +34,153 @@ class SecurityPersistenceManager {
 //             PUBLIC FUNCTIONS
 //===========================================
 SecurityPersistenceManager.prototype.getUser = function(oUser) {
-    // TODO: Return a user using properties corresponding to the provided object
-}
+    var sQuery = `SELECT * FROM ${this.database}.USER WHERE `;//uuid = "f02472-11e8-9406-080027d2c7dd";';
+    var aProperties = Object.keys(oUser).map(sKey => {
+        return `${sKey} = "${oUser[sKey]}"`;
+    })
+    sQuery += aProperties.join(" AND ") + ";";
+
+    return this._query(sQuery);
+
+
+
+
+    // var promise =
+    // this.connection.query(sQuery, function (error, results, fields) {
+    //     console.log("after query");
+    //
+    //     return new Promise((resolve, reject) => {
+    //         if (error) {
+    //             console.log("error");
+    //             reject(error);
+    //         } else {
+    //             console.log("success");
+    //             resolve(results, fields);
+    //         }
+    //     });
+    // });
+
+};
+SecurityPersistenceManager.prototype._query = function(sQuery, aParams) {
+
+    // var interval = 1000;
+    var counter = 0;
+    // var max = 10000;
+
+    var complete = false;
+    var promise;
+    //
+    // var timeout = setInterval(console.log("Hello"), 10);
+    // this.connection.query[util.promisify.custom] = (sQuery, aParams) => {
+    //     return new Promise((resolve, reject) => {
+    //         // doSomething(foo, resolve, reject);
+    //         this.connection.query(sQuery, (aParams || []), (error, results, fields) => {
+    //             if (error) {
+    //                 console.log("error");
+    //                 reject(error);
+    //             } else {
+    //                 console.log("success");
+    //                 resolve(results, fields);
+    //             }
+    //         });
+    //     });
+    // };
+    // var con = this.connection;
+    // console.log(con);
+
+    // var oConnection = this.connection.then(());
+    // const promisified = util.promisify(oConnection.query);
+    // return promisified(sQuery, (aParams || []));
+    // var prom = promisified(sQuery, (aParams || []));
+    // prom.then((result) => {
+    //     console.log(result);
+    // }, (error) => {
+    //     console.log(error);
+    // })
+
+
+
+
+
+
+
+    // console.log(this.state);
+    // // setTimeout(function(args) {
+    //
+    // do {
+    //     console.log(this.state);
+    // }
+    // while (this.state !== "complete")
+    //
+    // console.log(this.state);
+    // var prom = this.connection.query(sQuery, (aParams || []));
+    // prom.then((result) => {
+    //     console.log(result);
+    // }, (error) => {
+    //     console.log(error);
+    // })
+    // // }, 10000);
+
+
+
+
+
+    // console.log(this.connection.query.toString());
+
+    console.log(sQuery);
+    // try {
+    return new Promise((resolve, reject) => {
+        this.connection.query(sQuery, (aParams || []), (error, results, fields) => {
+            console.log("after query");
+
+                if (error) {
+                    console.log("error");
+                    reject(error);
+                } else {
+                    console.log("success");
+                    // console.log(results);
+                    // console.log(fields);
+                    resolve(results, fields);
+                }
+            });
+            complete = true;
+            console.log(complete);
+            console.log(promise);
+        });
+
+    // } catch (err) {
+    //     // console.log("err");
+    //     // console.log(err);
+    // }
+// console.log(complete);
+//     while (!complete) {
+//         counter++;
+//         // console.log(counter);
+//     }
+//     // console.log(promise);
+//
+//     if (complete) {
+//         console.log("complete");
+//         return promise;
+//     } else {
+//         console.log("Database query timed out");
+//         return new Promise().reject("Database query timed out");
+//     }
+
+
+
+
+
+
+    // setInterval(function() {
+    //     console.log("inside set interval");
+    //     } else if (counter <= max) {
+    //         console.log("} else if (counter <= max) {");
+    //         counter += interval;
+    // }, interval);
+
+};
+
 SecurityPersistenceManager.prototype.getRolesByUser = function(sUserId) {
     // TODO: Return an array of rolescorresponding to the provided user
 }
@@ -65,11 +221,12 @@ SecurityPersistenceManager.prototype.extendSession = function(oExtension) {
 //             PRIVATE FUNCTIONS
 //===========================================
 SecurityPersistenceManager.prototype._missingParameter = function(sParameter) {
-    console.error(`
-        === ERROR: ===
-        Missing ${sParameter} parameter in the constructor of SecurityPersistenceManager.
-        Ensure that process.env.database.${sParameter} is set or the parameter is otherwise provided.
-    `);
+throw Error(`
+---
+Missing ${sParameter} parameter in the constructor of SecurityPersistenceManager.
+Ensure that process.env.com.etauker.security.db.${sParameter} is set or the parameter is otherwise provided.
+---
+`);
 }
 
 module.exports = SecurityPersistenceManager;
---
+++ test/persistence/SecurityPersistenceManagerTest.spec.js
@@ -0,0 +1,70 @@
+var SecurityPersistenceManager = require("../../persistence/SecurityPersistenceManager.js");
+// var oConfig = {};
+// var oManager = {};
+//
+//
+//
+// oConfig = {
+//     host: "test host",
+//     user: "test user",
+//     password: "test password",
+//     database: "test database"
+// };
+oManager = new SecurityPersistenceManager({
+    host: "localhost",
+    user: "securityAdmin",
+    password: "securityAdmin",
+    database: "etauker_security"
+});
+//
+// oConfig = {SecurityPersistenceManagerTest
+//     user: "test user",
+//     password: "test password",
+//     database: "test database"
+// };
+// oManager = new SecurityPersistenceManager(oConfig);
+//
+// oConfig = {
+//     host: "test host",
+//     password: "test password",
+//     database: "test database"
+// };
+// oManager = new SecurityPersistenceManager(oConfig);
+//
+//
+// oConfig = {
+//     host: "test host",
+//     user: "test user",
+//     database: "test database"
+// };
+// oManager = new SecurityPersistenceManager(oConfig);
+//
+//
+// oConfig = {
+//     host: "test host",
+//     user: "test user",
+//     password: "test password"
+// };
+// oManager = new SecurityPersistenceManager(oConfig);
+
+
+describe("SecurityPersistenceManager", function() {
+    it("should retrieve a user with specified username", function() {
+
+        oManager.getUser({
+            uuid: "f024444f-b872-11e8-9406-080027d2c7dd"
+        }).then((oResult, oFields) => {
+            console.log("oResult");
+            console.log(JSON.stringify(oResult));
+            console.log(JSON.stringify(oFields));
+
+            expect(oResult).not.toBe(null);
+            expect(oResult).not.toBe(undefined);
+            expect(oResult.length).toBe(1);
+        }, (oError) => {
+            console.log("oError");
+            console.log(JSON.stringify(oError));
+            expect(false);
+        });
+    });
+});
