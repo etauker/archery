ae93c957a04639aaf63199c3d5efa99137e30782
Tautvydas Kersulis <etauker@gmail.com>
2018-11-06 22:53:36 +0000

    Added inject script.

---
+++ scripts/inject.js
@@ -0,0 +1,35 @@
+let i18nFiles = [
+    "./presentation/com.etauker.security/webapp/i18n/i18n.properties",
+    "./presentation/com.etauker.glucose/webapp/i18n/i18n.properties"
+]
+
+const fs = require('fs');
+
+i18nFiles.forEach(file => {
+
+    let content = "";
+    if (fs.existsSync(file)) {
+
+        try { content = fs.readFileSync(file, 'utf8'); }
+        catch (oError) {
+            console.warn(`[error] Error occured while reading ${file}.`);
+            console.warn("[error] " + oError);
+        }
+
+        if (process.env.PROTOCOL)
+            content = content.replace(/env\.PROTOCOL=.*/g, `PROTOCOL=${process.env.PROTOCOL}`)
+        if (process.env.HOST)
+            content = content.replace(/env\.HOST=.*/g, `HOST=${process.env.HOST}`)
+        if (process.env.PORT)
+            content = content.replace(/env\.PORT=.*/g, `PORT=${process.env.PORT}`)
+
+        console.log("--- Updated i18n File ---")
+        console.log(content)
+
+        try { fs.writeFileSync(file, content); }
+        catch (oError) {
+            console.warn(`[error] Error occured while writeing to ${file}.`);
+            console.warn("[error] " + oError);
+        }
+    }
+})
