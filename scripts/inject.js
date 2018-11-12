f68b3772cde9ca34ef98656ec8950c5db1af76c1
Tautvydas Kersulis <etauker@gmail.com>
2018-11-12 19:01:45 +0000

    Automated detection of i18n files.

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,34 +1,77 @@
-let i18nFiles = [
-    "./presentation/com.etauker.glucose/webapp/dependencies/com.etauker.security/i18n/i18n.properties",
-    "./presentation/com.etauker.glucose/webapp/i18n/i18n.properties"
-]
-
 const fs = require('fs');
+const path = require('path');
+let i18nFiles = [];
+let i18nRegex = /.*i18n.*\.properties/g;
 
+console.log(`--- Detecting i18n files ---`);
+findFiles('./presentation', i18nRegex, i18nFiles);
+
+console.log(`--- Injecting environment variables ---`)
 i18nFiles.forEach(file => {
 
     let content = "";
     if (fs.existsSync(file)) {
 
-        try { content = fs.readFileSync(file, 'utf8'); }
+        // Read file
+        try {
+            content = fs.readFileSync(file, 'utf8');
+        }
         catch (oError) {
             console.warn(`[error] Error occured while reading ${file}.`);
-            console.warn("[error] " + oError);
+            console.warn(`[error] ${JSON.stringify(oError)}`);
         }
 
-        if (process.env.PROTOCOL)
+        // Inject environment variables
+        let injected = false;
+        if (process.env.PROTOCOL) {
             content = content.replace(/env\.PROTOCOL=.*/g, `env.PROTOCOL=${process.env.PROTOCOL}`)
-        if (process.env.HOST)
+            injected = true;
+        }
+        if (process.env.HOST) {
             content = content.replace(/env\.HOST=.*/g, `env.HOST=${process.env.HOST}`)
-        if (process.env.PORT)
+            injected = true;
+        }
+        if (process.env.PORT) {
             content = content.replace(/env\.PORT=.*/g, `env.PORT=${process.env.PORT}`)
+            injected = true;
+        }
+        if (injected) {
+            console.log(`Injected into ${file}`)
+        }
 
-        console.log("--- Updated i18n File ---")
-
-        try { fs.writeFileSync(file, content); }
+        // Write file
+        try {
+            fs.writeFileSync(file, content);
+        }
         catch (oError) {
-            console.warn(`[error] Error occured while writeing to ${file}.`);
+            console.warn(`[error] Error occured while writing to ${file}.`);
             console.warn("[error] " + oError);
         }
     }
-})
+});
+
+
+// =============================================
+//               HELPER FUNCTIONS
+// =============================================
+function findFiles(rootDirectory, regex, array) {
+
+    if (!fs.existsSync(rootDirectory)){
+        console.log(`-- ${rootDirectory} not found!`);
+        return;
+    }
+
+    const files = fs.readdirSync(rootDirectory);
+    files.forEach(file => {
+        const filename = path.join(rootDirectory, file);
+        const stat = fs.lstatSync(filename);
+
+        if (stat.isDirectory()) {
+            findFiles(filename, regex, array);
+        }
+        else if (regex.test(filename)) {
+            console.log(`Detected: ${filename}`);
+            array.push(filename);
+        };
+    });
+};
