4a99d63b65e475cbb2d27703180fe626f3ae4048
Tautvydas Kersulis <t.kersulis1@nuigalway.ie>
2019-01-27 00:39:21 +0000

    https://trello.com/c/CJwVmkZB Set base-href in index.html to / Added --base-href build parameter

--- dependencies/com.etauker.glucose.charts/frontend/src/app/app.component.ts
+++ dependencies/com.etauker.glucose.charts/frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'Glucose Charts';
+    title = 'Glucose Charts';
+    constructor() { }
+    ngOnInit() { }
 }
