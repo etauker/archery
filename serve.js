0cae16d46591a93602397c58f8cb198ef4e447f8
Tautvydas Kersulis <t.kersulis1@nuigalway.ie>
2019-01-30 22:56:55 +0000

    Added configuration for security logout

--- serve.js
+++ serve.js
@@ -13,16 +13,19 @@ global.REALM = "etauker.com";
 const app = express();
 const port = process.env.PORT || 8888;
 const securityLoginWebappEndpoint = '/login';
+const securityLogoutWebappEndpoint = '/logout';
 const archeryWebappEndpoint = '/archery';
 const glucoseWebappEndpoint = '/glucose';
 const glucoseChartsWebappEndpoint = '/glucose-charts';
 // Disabled until: v1.3
 // const archeryWebappDirectory = __dirname + "/presentation/com.etauker.archery/archery-ui5/webapp";
 const securityLoginWebappDirectory = __dirname + "/dependencies/com.etauker.security.login/frontend/webapp";
+const securityLogoutWebappDirectory = __dirname + "/dependencies/com.etauker.security.logout/frontend/webapp";
 const glucoseWebappDirectory = __dirname + "/presentation/com.etauker.glucose/webapp";
 const glucoseChartsWebappDirectory = __dirname + "/dependencies/com.etauker.glucose.charts/frontend/dist";
 const modules = [ "data", "persistence", "logic", "service", "utils" ];
 const dependencies = [
+    "com.etauker.security.login/frontend"
     "com.etauker.security",
     "com.etauker.archery",
     "com.etauker.glucose",
@@ -45,6 +48,7 @@ console.log("Port: " + port);
 // Disabled until: v1.3
 // console.log("Archery webapp endpoint: " + archeryWebappEndpoint);
 console.log("Security Login webapp endpoint: " + securityLoginWebappEndpoint);
+console.log("Security Logout webapp endpoint: " + securityLogoutWebappEndpoint);
 console.log("Glucose Transactions webapp endpoint: " + glucoseWebappEndpoint);
 console.log("Glucose Charts webapp endpoint: " + glucoseChartsWebappEndpoint);
 
@@ -86,6 +90,7 @@ app.use("/glucose", glucoseRouter)
 // Disabled until: v1.3
 // app.use(archeryWebappEndpoint, express.static(archeryWebappDirectory));
 app.use(securityLoginWebappEndpoint, express.static(securityLoginWebappDirectory));
+app.use(securityLogoutWebappEndpoint, express.static(securityLogoutWebappDirectory));
 app.use(glucoseWebappEndpoint, express.static(glucoseWebappDirectory));
 app.use(glucoseChartsWebappEndpoint, express.static(glucoseChartsWebappDirectory));
 
@@ -105,5 +110,6 @@ app.all(`${glucoseChartsWebappEndpoint}/*`, function(req, res, next) {
 app.listen(port);
 console.log("--- App listening ---");
 console.log("Security Login: " + securityLoginWebappEndpoint);
+console.log("Security Logout: " + securityLogoutWebappEndpoint);
 console.log("Glucose Transactions: " + glucoseWebappEndpoint);
 console.log("Glucose Charts: " + glucoseChartsWebappEndpoint);
