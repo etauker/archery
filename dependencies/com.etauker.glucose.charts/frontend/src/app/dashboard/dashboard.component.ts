7b6be52e796a9ffe1439e2e1bb3260cbb72a39ff
Tautvydas Kersulis <t.kersulis1@nuigalway.ie>
2019-02-05 23:56:34 +0000

    22: Refactored charts service https://trello.com/c/TPNwSlnB
    Tidied up charts service.
    Added some mock data.
    Added meal period configuration json.
    Changed chart in dashboard component.

--- dependencies/com.etauker.glucose.charts/frontend/src/app/dashboard/dashboard.component.ts
+++ dependencies/com.etauker.glucose.charts/frontend/src/app/dashboard/dashboard.component.ts
@@ -10,7 +10,33 @@ import { ChartsService } from '../service/charts/charts.service';
 })
 export class DashboardComponent implements OnInit {
 
-    constructor(private _service: ChartsService) {}
+    readingData = [];
+    readingDataPointColors = [];
+
+    constructor(private charts: ChartsService) {
+
+        let today = new Date();
+        today.setDate(today.getDate() - 7);
+        today.setHours(0);
+        today.setMinutes(0);
+        today.setMilliseconds(0);
+        let start = today.valueOf();
+        today.setDate(today.getDate() + 8);
+        let end = today.valueOf();
+        // console.log(start);
+        // console.log(end);
+
+
+        let subscription = charts.getChartData('chart-1', start, end).subscribe(
+            (value: any) => {
+                // console.log(value);
+                this.readingData = value.readingData;
+                this.readingDataPointColors = value.readingDataPointColors;
+            },
+            (error) => console.log(error),
+            () => this.showCharts()
+        )
+    }
 
     selected = 'custom';
     // data: {
@@ -33,8 +59,10 @@ export class DashboardComponent implements OnInit {
     chart: any;
     chart2: any;
 
-    ngOnInit() {
-      // this.data = this._service.getChartData('chart-1');
+    ngOnInit() { }
+
+    private showCharts() {
+      // this.data = this.charts.getChartData('chart-1');
       let ctx = document.getElementById("canvas");
       let ctx2 = document.getElementById("canvas2");
 
@@ -43,18 +71,79 @@ export class DashboardComponent implements OnInit {
           options: {
               responsive: true,
               maintainAspectRatio: false,
-              onResize: (instance, newSize) => console.log(newSize)
+              onResize: (instance, newSize) => console.log(newSize),
+              scales: {
+                  yAxes: [{
+                      id: 'readings',
+                      type: 'linear',
+                      position: 'left',
+                      scaleLabel: {
+                          labelString: 'Readings',
+                          display: true
+                      },
+                      ticks: {
+                          suggestedMax: 20,
+                          suggestedMin: 0,
+                          stepSize: 4
+                      }
+                  }],
+                  xAxes: [{
+                      id: 'time',
+                      type: 'time',
+                      distribution: 'linear',
+                      scaleLabel: {
+                          labelString: 'Time',
+                          display: true
+                      },
+                      time: {
+                          unit: 'day',
+                          stepSize: 1
+
+                      }
+                  }]
+              },
+              tooltips: {
+                  callbacks: {
+                      label: function(tooltipItem, data) {
+                          var datasetLabel = data.datasets[tooltipItem.datasetIndex].label || '';
+                          let dataset = data.datasets[tooltipItem.datasetIndex];
+                          let datapoint = dataset.data[tooltipItem.index];
+                          let labelNote = datapoint.label || '';
+                          let reading = datapoint.y;
+                          let label = '';
+
+                          if (reading) {
+                              label = `Reading: ${reading}`
+                          }
+                          if (labelNote) {
+                              label += `, Note: ${labelNote}`
+                          }
+
+                          return label;
+                      }
+                  }
+              }
           },
           data: {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            // labels: ["January", "February", "March", "April", "May", "June", "July"],
             datasets: [{
-                label: "My First dataset",
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45],
+                label: 'Readings',
+                yAxisID: 'readings',
+                xAxisID: 'time',
+                data: this.readingData,
+                pointBackgroundColor: this.readingDataPointColors
             }]
+            // datasets: [{
+            //     label: "My First dataset",
+            //     backgroundColor: 'rgb(255, 99, 132)',
+            //     borderColor: 'rgb(255, 99, 132)',
+            //     data: [0, 10, 5, 2, 20, 30, 45],
+            // }]
         }
+
+
     });
+    // this.charts.getChartData('chart-1');
     this.chart.canvas.parentNode.style.height = '256px';
     this.chart.resize();
 
